657a9af36bedcc14b8ada52e95abead531ef01bd
Ben Church <ben.douglas.church@gmail.com>
2019-10-25 13:04:02 -0600

    Misc

--- app/Main.hs
+++ app/Main.hs
@@ -5,24 +5,76 @@ import Web.Spock
 import Web.Spock.Config
 
 import Control.Monad.Trans
-import Data.Monoid
+-- import Data.Monoid
 import Data.IORef
 import qualified Data.Text as T
+import Data.Time.Clock
 
 data MySession = EmptySession
-data MyAppState = DummyAppState (IORef Int)
+newtype MyAppState = DummyAppState (IORef Int)
 
 main :: IO ()
-main =
-    do ref <- newIORef 0
-       spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
-       runSpock 8080 (spock spockCfg app)
+main = do
+  ref <- newIORef 0
+  spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
+  runSpock 8000 (spock spockCfg app)
 
+-- | This is the app
 app :: SpockM () MySession MyAppState ()
-app =
-    do get root $
-           text "Hello World!"
-       get ("hello" <//> var) $ \name ->
-           do (DummyAppState ref) <- getState
-              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
-              text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+app = do
+  get root handleRoot
+  get ("hello" <//> var) handleHello
+  get "time" returnTime
+
+returnTime :: ActionCtxT ctx (WebStateM () MySession MyAppState) a
+returnTime =  do
+    time <- liftIO getCurrentTime
+    -- return time
+    text $ T.pack (show time)
+    -- (getCurrentTime :: IO UTCTime) >>= (\time :: UTCTime) ->
+    --   (text "show time" :: ActionCtxT ctx ...)
+
+-- test :: IO a -> Writer String a
+-- fmap test serverIO
+-- wrapper >>= continuation :: m a -> (a -> m b) -> m b
+
+-- liftIO :: IO a -> m a
+-- generalizeIO
+
+-- MoandIO -- IOable
+
+
+-- instance MonadIO (RIO cfg a) where
+--   liftIO wrapped =
+--     -- how to wrap this in Maybe a
+--     -- And later we'll have some way of converting m a -> IO a
+
+--   runRIO config
+--   m a -> IO a
+
+-- getCurrentTime.then(time => {
+--   return(text(time))
+-- })
+
+-- do
+--   a <- foo
+--   b <- bar a
+
+-- const a = await foo
+-- const b = await bar(a)
+
+-- Right 42 >>= \_ ->
+--   Just 43
+
+  -- Just 43
+
+
+
+handleRoot :: ActionCtxT ctx (WebStateM () MySession MyAppState) a
+handleRoot = text "Hello World!"
+
+handleHello :: T.Text -> ActionCtxT ctx (WebStateM () MySession MyAppState) a
+handleHello name = do
+    (DummyAppState ref) <- getState
+    visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
+    text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
